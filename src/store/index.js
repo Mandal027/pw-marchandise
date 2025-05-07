e23fefd0f21b9ad1a844a0f44c44a9abd68f70ba
Nilesh <kumarnilesh010524@gmail.com>
2025-05-07 22:54:25 +0530

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { proxy } from "valtio";
 
 const state = proxy({
     intro: true,
-    color: "#EFBD48",
+    color: "#160D08",
     isLogoTexture: true,
     isfullTexture: false,
     logoDecal: "./Tshirt_logo.png",
